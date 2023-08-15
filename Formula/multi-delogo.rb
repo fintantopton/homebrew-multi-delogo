b29d7973a6bae34ea794816adce0f95050c150dc
fintantopton <40918749+fintantopton@users.noreply.github.com>
2023-08-15 21:10:07 +1200

    Update multi-delogo.rb

--- Formula/multi-delogo.rb
+++ Formula/multi-delogo.rb
@@ -1,8 +1,8 @@
 class MultiDelogo < Formula
   desc "Detect and remove logos from videos"
   homepage "https://github.com/wernerturing/multi-delogo"
-  url "https://github.com/wernerturing/multi-delogo.git",
-      :branch => "opencv4"
+  url "https://github.com/fintantopton/homebrew-multi-delogo.git",
+      :branch => "master"
   version "2.3.0"
   sha256 "80ff11873ec0e73d9e38b0eb2ffb1586621f0b804093b990e49fdb546476ed6e"
 
